2877e76eccb9686a0c09a7aa80cd7d6d85356ffb
Alex Ku <alex.ku96@outlook.com>
2019-01-30 17:51:11 -0500

    Updated Project. Comments included.

--- JournalLibrary/JournalLibrary/Profile.cs
+++ JournalLibrary/JournalLibrary/Profile.cs
@@ -10,33 +10,64 @@ namespace JournalLibrary
 {
     public class Profile
     {
+        //Connection (Like a bridge)
         static string connection = "Data Source=DESKTOP-2PSBHKH\\SQLEXPRESS;Initial Catalog=JournalWebsite;Integrated Security=True";
+        //Connecting to your database
         static SqlConnection JournalWebsite = new SqlConnection(connection);
-
+        //edit profile method. Two original inputs with two new input paramenters. At the end of this method...it will receive either a true or false value.
         public static bool editProfile(string username, string password, string newusername, string newpassword)
         {
+            //SQL querey to SELECT loginid, username, and password with WHERE coditions
             string SELECT = $"SELECT [LogInID], [UserName], [Password] FROM [User info] WHERE [UserName] = '{username}' AND [Password] = '{password}'";
 
+            /*Used the data adapter to fill the upcoming dataset. REMEMBER, data adapter is only to fill data sets or tables. 
+             Cannot be used to execute other commands.*/
             SqlDataAdapter edit = new SqlDataAdapter(SELECT, JournalWebsite);
 
-            DataTable table = new DataTable();
+            //Created a data set object to store my data when the query is executed
+            DataSet result = new DataSet();
 
-            edit.Fill(table);
+            //The data in the edit then fills the result data set
+            edit.Fill(result);
 
-            if (table.Rows.Count == 1)
+            /*This statement looks tricky but its simple..."result" is the dataset that now has all the data that the previous 
+             query updated. Then it is accessed to its Table at element 0. Its 0 because arrays element 0 is 1. then it is accessed to its 
+             Rows at element 0 with the SQL column name. Then it is converted into a string. The string is stored in Susername. This is the original 
+             username data.*/
+            string Susername = result.Tables[0].Rows[0]["UserName"].ToString();
+
+            //If Susername (original) is equal to the new username. Return false
+            if (Susername == newusername)
             {
                 return false;
             }
+            //If the new password is blank or less than 1. Return false.
             else if (newpassword == "" || newpassword.Length < 1)
             {
                 return false;
             }
+            //If everything is okay. Execute the following code.
             else
             {
-                string SlogID = table.Rows[0]["LogInId"].ToString();
+                //The statement allows the logInId to be stored in slogInID. Need it for the upcoming query.
+                string SlogID = result.Tables[0].Rows[0]["LogInId"].ToString();
+
+                //A query that allows the user to upadte its username and password according to its logInID
                 string UPDATE = $"UPDATE [User info] SET [UserName] = '{newusername}', [Password] = '{newpassword}' WHERE [LogInID] ={SlogID}";
 
+                //Stores UPDATE query into the database as a command.
                 SqlCommand command = new SqlCommand(UPDATE, JournalWebsite);
+
+                //Need to open the database in order to execute command.
+                JournalWebsite.Open();
+
+                //Execute command.
+                command.ExecuteNonQuery();
+
+                //Cannot finish without closing your database. Close databse.
+                JournalWebsite.Close();
+
+                //Operation is complete. Return true.
                 return true;
             }
         }
--- JournalWebsite/MainWindow.xaml.cs
+++ JournalWebsite/MainWindow.xaml.cs
@@ -23,19 +23,40 @@ namespace JournalWebsite
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Do not worry about this. This is just initialize your page. In another words, starting your page.
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //Log in button click event. The codes will execute when the log in button is clicked.
         private void LogButton_Click(object sender, RoutedEventArgs e)
         {
-            bool result = 
+            //Assigning the logIn with a bool by calling the login method using Ubox and Pbox in the xaml page as arguements. 
+            bool LogIn = Access.login(Ubox.Text.Trim(), Pbox.Text.Trim());
+
+            //If the login is true. Execute the following code.
+            if (LogIn == true)
+            {
+                /*"windownav" is the name of the frame from MainWindow.xaml. Source basically means that this frame is the main source to navigate through.
+                 We are navigating through a new URI, Uniformed Resource Identifier aka our next page. Then we put our page name. Then we put urikind 
+                 which means the different kinds of URI. The URI we are using is a relative to our main source. Well from what I know.*/
+                windownav.Source = new Uri("MainPage.xaml", UriKind.Relative);
+            }
+            else
+            {
+                //If the login is false, then make the textblock opacity to 100.
+                Invalid.Foreground.Opacity = 100;
+            }
     
         }
-
+        //Register button click event. The codes will execute when the register button is clicked.
         private void RegButton_Click(object sender, RoutedEventArgs e)
-        {        
+        {
+            /*"windownav" is the name of the frame from MainWindow.xaml. Source basically means that this frame is the main source to navigate through.
+                We are navigating through a new URI, Uniformed Resource Identifier aka our next page. Then we put our page name. Then we put urikind 
+                which means the different kinds of URI. The URI we are using is a relative to our main source. Well from what I know.*/
+            windownav.Source = new Uri("Register.xaml", UriKind.Relative);
         }
     }
 }
